ca331aac24f93acb83024015a45c61fe27251cb2
Felipe-Rodr <lipe-rodrigo@hotmail.com>
2022-07-19 11:11:44 -0300

    Projeto finalizado.

--- Scripts/Scripts.js
+++ Scripts/Scripts.js
@@ -1,11 +1,18 @@
 function Converter(){
     NumeroBinario = document.getElementById("NumeroBinario").value;
     ValorDecimal = 0;
+    if(NumeroBinario.length > 8){
+        document.getElementById('resultado').innerHTML = 'Valor inserido invalido.'
+        return;
+    }
     for(let i=0; i<NumeroBinario.length; i++){
         if(NumeroBinario[i] == 0){
             continue
-        } else {
+        } else if(NumeroBinario[i] == 1){
             ValorDecimal += Math.pow(2,i);
+        } else {
+            document.getElementById('resultado').innerHTML = 'Valor inserido invalido.'
+            return;
         }
     }
     document.getElementById('resultado').innerHTML = 'Valor Decimal: ' + ValorDecimal;
